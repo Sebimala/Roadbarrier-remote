d1c589fed7062446388a5f4990b63fa20e38c1f2
Sebimala <86785937+Sebimala@users.noreply.github.com>
2025-01-31 15:58:05 +0100

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,9 +1,23 @@
 // Opens the barrier
 input.onButtonPressed(Button.A, function () {
     radio.sendNumber(1)
+    basic.showLeds(`
+        . . # . .
+        . # # # .
+        # . # . #
+        . . # . .
+        . . # . .
+        `)
 })
 // Closes the Barrier
 input.onButtonPressed(Button.B, function () {
     radio.sendNumber(2)
+    basic.showLeds(`
+        . . # . .
+        . . # . .
+        # . # . #
+        . # # # .
+        . . # . .
+        `)
 })
 radio.setGroup(1)

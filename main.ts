6886cf69d79fbde351e43c75486aba41327ef821
Sebimala <86785937+Sebimala@users.noreply.github.com>
2025-01-31 15:25:42 +0100

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,9 @@
-basic.forever(function () {
-	
+// Opens the barrier
+input.onButtonPressed(Button.A, function () {
+    radio.sendNumber(1)
 })
+// Closes the Barrier
+input.onButtonPressed(Button.B, function () {
+    radio.sendNumber(2)
+})
+radio.setGroup(1)
